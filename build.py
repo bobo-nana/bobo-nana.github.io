b981803b948a9b8a545421eb088da7bf732cbbfd
Ironhead <ironhead.chuang@gmail.com>
2025-03-08 14:22:43 +0800

    feat: Initial commit.

---
+++ build.py
@@ -0,0 +1,498 @@
+import copy
+import pathlib
+import re
+
+import bs4
+import yaml
+
+
+class Site:
+
+    def __init__(self, root_path: pathlib.Path) -> None:
+
+        if not root_path.is_dir():
+            raise ValueError(f"Invalid path. root_path: {root_path}")
+
+        self._nodes = {}
+
+        for yaml_path in root_path.glob("**/_*.yaml"):
+            html_name = yaml_path.name[1:-4] + "html"
+            html_path = yaml_path.with_name(html_name)
+
+            with (root_path / yaml_path).open("r") as yaml_file:
+                content = yaml.safe_load(yaml_file)
+
+            if content.get("type") == "data":
+                node = DataNode(html_path, content)
+            else:
+                node = BaseNode(root_path, html_path, Node.create(html_path, content))
+
+            self._nodes[node.path] = node
+
+    def get_attr(self, path: str) -> dict:
+        if path not in self._nodes:
+            raise ValueError(f"Invalid path. path: {path}")
+
+        return self._nodes[path].attr
+
+
+class Node:
+
+    """
+    - Every file with name starting with underscore is a node.
+    - A node contains data and children.
+        - data: The dictionary without the "children" key.
+        - children: The values of the "children" key. Representing the child nodes of the node.
+    """
+
+    REGISTERED_NODE_TYPES = {}
+
+    @staticmethod
+    def register(node_class: type) -> type:
+        node_class_name = "".join([
+            ("_" if i > 0 and c.isupper() else "") + c.lower()
+            for i, c in enumerate(node_class.__name__)
+        ])
+
+        if node_class_name.startswith("_"):
+            node_class_name = node_class_name[1:]
+        if node_class_name.endswith("_node"):
+            node_class_name = node_class_name[:-5]
+
+        Node.REGISTERED_NODE_TYPES[node_class_name] = node_class
+
+        return node_class
+
+    @staticmethod
+    def create(path: pathlib.Path, data: dict) -> "Node":
+        if "type" not in data:
+            raise ValueError(f"Invalid data. data: {data}")
+
+        node_class = Node.REGISTERED_NODE_TYPES[data["type"]]
+
+        return node_class(path, data)
+
+    @staticmethod
+    def process_text(text: str) -> str:
+        text = re.sub(r"__(.*?)__", r"<i>\1</i>", text)
+        text = re.sub(r"\*\*(.*?)\*\*", r"<b>\1</b>", text)
+        text = re.sub(r"~~(.*?)~~", r"<s>\1</s>", text)
+
+        return text
+
+    @staticmethod
+    def process_url(html_path: pathlib.Path, url: str) -> str:
+        if url.startswith("http://") or url.startswith("https://"):
+            return url
+
+        if url.startswith("/"):
+            return url
+
+        return str(html_path.parent / url)
+
+    def __init__(self, path: pathlib.Path, data: dict) -> None:
+        children = data.pop("children", [])
+
+        self._path = path
+        self._attr = data
+        self._children = [Node.create(path, child) for child in children]
+
+    @property
+    def attr(self) -> dict:
+        return copy.deepcopy(self._attr)
+
+    @property
+    def path(self) -> str:
+        return "/" + str(self._path)
+
+    def render(self, site: Site) -> str:
+        return "".join([child.render(site) for child in self._children])
+
+    def render_children(self, site: Site) -> str:
+        return "".join([child.render(site) for child in self._children])
+
+
+@Node.register
+class DataNode(Node):
+    pass
+
+
+@Node.register
+class BaseNode(Node):
+
+    def __init__(self, root_path: pathlib.Path, html_path: pathlib.Path, child: Node) -> None:
+        super().__init__(html_path, {})
+
+        self._root_path = root_path
+        self._html_path = html_path
+        self._children = [child]
+
+    def render(self, site: Site) -> str:
+        head = HeadNode("/", {}).render(site)
+        header = HeaderNode("/", {}).render(site)
+        footer = FooterNode("/", {}).render(site)
+        content = "".join([child.render(site) for child in self._children])
+
+        html = f"""
+            <!DOCTYPE html>
+            <html lang="en" data-bs-theme="dark">
+                {head}
+                <body>
+                { header }
+                <main class="container w-75">
+                    { content }
+                </main>
+                { footer }
+                </body>
+            </html>
+            """
+        
+        html_formatter = bs4.formatter.HTMLFormatter(indent=4)
+
+        html = bs4.BeautifulSoup(html, "html.parser").prettify(formatter=html_formatter)
+
+        with (self._root_path / self._html_path).open("w") as html_file:
+            html_file.write(html)
+
+        return html
+
+
+@Node.register
+class HeadNode(Node):
+    def render(self, site: Site) -> str:
+        css_timeline = f"""
+            .timeline {{
+                border-left: 1px solid hsl(0, 0%, 80%);
+                position: relative;
+                list-style: none;
+                margin-left: 25px;
+                margin-right: 25px;
+            }}
+
+            .timeline .timeline-item {{
+                position: relative;
+            }}
+
+            .timeline .timeline-item:after {{
+                position: absolute;
+                display: block;
+                top: 0;
+            }}
+
+            .timeline .timeline-icon {{
+                position: absolute;
+                left: -48px;
+                background-color: hsl(217, 88.2%, 10%);
+                color: hsl(217, 88.8%, 35.1%);
+                border-radius: 50%;
+                border: 1px solid hsl(217, 88.2%, 80%);
+                height: 31px;
+                width: 31px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }}
+        """
+
+        return f"""
+            <head>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="description" content="description">
+
+                <title>{site.get_attr("/site.html").get("name")}</title>
+
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                <style>
+                    body {{
+                        min-height: 75rem;
+                        padding-top: 4.5rem;
+                    }}
+
+                    {css_timeline}
+                </style>
+            </head>
+        """
+
+
+@Node.register
+class HeaderNode(Node):
+
+    def render(self, site: Site) -> str:
+        attr = site.get_attr("/site.html")
+
+        site_name = attr.get("name")
+
+        page_list = []
+
+        for page in attr.get("pages", []):
+            if "link" in page:
+                page_list.append(f"""
+                    <li class="nav-item">
+                        <a class="nav-link" href="{page.get("link")}">{page.get("name")}</a>
+                    </li>
+                    """)
+            else:
+                sub_page_list = "".join([
+                    f"""
+                    <li><a class="dropdown-item" href="{sub_page.get("link")}">{sub_page.get("name")}</a></li>
+                    """
+                    for sub_page in page.get("list", [])
+                ])
+
+                head = f"""
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {page.get("name")}
+                    </a>
+                """
+
+                page_list.append(f"""
+                    <li class="nav-item dropdown">
+                        {head}
+                        <ul class="dropdown-menu">
+                            {sub_page_list}
+                        </ul>
+                    </li>
+                """)
+
+        return f"""
+            <nav class="navbar fixed-top navbar-expand-md bg-body-tertiary">
+                <div class="container w-75">
+
+                    <a class="navbar-brand" href="/">
+                        {site_name}
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            {''.join(page_list)}
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        """
+
+
+@Node.register
+class FooterNode(Node):
+
+    def render(self, site: Site) -> str:
+        return ""
+
+
+@Node.register
+class PageNode(Node):
+    pass
+
+
+@Node.register
+class PostNode(Node):
+    def render(self, site: Site) -> str:
+        title = self.attr.get("title")
+        date = self.attr.get("date", "????-??")
+        authors = self.attr.get("authors", [])
+        tags = self.attr.get("tags", [])
+
+        authors = (", " if authors else "").join(authors)
+        tags = map(lambda tag: f"""<span class="badge rounded-pill text-bg-info">{tag}</span>""", tags)
+        tags = (" " if tags else "").join(tags)
+
+        content = super().render(site)
+
+        return f"""
+            <h1>{title}</h1>
+
+            <div class="mb-4">
+                <span>
+                    <i class="bi bi-calendar-event" style="font-size: 1rem; color: cornflowerblue;"></i>
+                    {date}
+                </span>
+
+                <span class="ms-3">
+                    <i class="bi bi-person" style="font-size: 1rem; color: cornflowerblue;"></i>
+                    {authors}
+                </span>
+
+                <span class="ms-3">
+                    <i class="bi bi-tags" style="font-size: 1rem; color: cornflowerblue;"></i>
+                    {tags}
+                </span>
+            </div>
+
+            {content}
+        """
+
+
+@Node.register
+class H1Node(Node):
+    def render(self, site: Site) -> str:
+        return f"<h1 class=\"mb-2\">{self.attr.get('text')}</h1>"
+
+
+@Node.register
+class H2Node(Node):
+    def render(self, site: Site) -> str:
+        return f"<h2 class=\"mb-2\">{self.attr.get('text')}</h2>"
+
+
+@Node.register
+class H3Node(Node):
+    def render(self, site: Site) -> str:
+        return f"<h3 class=\"mb-2\">{self.attr.get('text')}</h3>"
+
+
+@Node.register
+class H4Node(Node):
+    def render(self, site: Site) -> str:
+        return f"<h4 class=\"mb-2\">{self.attr.get('text')}</h4>"
+
+
+@Node.register
+class H5Node(Node):
+    def render(self, site: Site) -> str:
+        return f"<h5 class=\"mb-2\">{self.attr.get('text')}</h5>"
+
+
+@Node.register
+class H6Node(Node):
+    def render(self, site: Site) -> str:
+        return f"<h6 class=\"mb-2\">{self.attr.get('text')}</h6>"
+
+
+@Node.register
+class PNode(Node):
+    def render(self, site: Site) -> str:
+        return f"<p class=\"mb-3\">{self.attr.get('text')}</p>"
+
+
+@Node.register
+class DividerNode(Node):
+    def render(self, site: Site) -> str:
+        return f"<hr class=\"my-4\">"
+
+
+@Node.register
+class ImgNode(Node):
+    def render(self, site: Site) -> str:
+        attr = self.attr
+        src = attr.get("src", "#")
+        alt = attr.get("alt", "")
+        return f"""<img src="{src}" class="img-fluid w-100 rounded pb-4" alt="{alt}">"""
+
+
+@Node.register
+class TimelineNode(Node):
+    def render(self, site: Site) -> str:
+        return f"""
+        <div class="py-5">
+            <ul class="timeline">
+                {''.join([child.render(site) for child in self._children])}
+            </ul>
+        </div>
+        """
+
+@Node.register
+class TimelineItemNode(Node):
+    def render(self, site: Site) -> str:
+        attr = self.attr
+
+        icon = f"""<span class="timeline-icon">{attr.get("icon", "✅")}</span>"""
+
+        name = attr.get("name", "")
+        date = attr.get("date", "")
+        time = attr.get("time", "")
+        pict = attr.get("pict", "")
+        text = attr.get("text", "")
+        children = self.render_children(site)
+
+        if name:
+            name = f"""<h5 class="fw-bold">{name}</h5>"""
+
+        if date:
+            stamp = f"""<i class="bi bi-calendar-event" style="font-size: 1rem; color: dimgrey;"></i> {date}"""
+
+            if time:
+                stamp += f" {time}"
+        elif time:
+            stamp = f"""<i class="bi bi-clock" style="font-size: 1rem; color: dimgrey;"></i> {time}"""
+        else:
+            stamp = ""
+
+        if pict:
+            pict = f"""<img src="{pict}" class="img-fluid w-100 rounded pb-4" alt="{text}">"""
+
+        if text:
+            text = f"""<p>{text}</p>"""
+
+        return f"""
+            <li class="timeline-item mb-5">
+                {icon}
+                {name}
+                <p class="text-muted pb-2 fw-bold">
+                    {stamp}
+                </p>
+                {pict}
+                {text}
+                {children}
+            </li>
+        """
+
+
+@Node.register
+class TableNode(Node):
+    def render(self, site: Site) -> str:
+        attr = self.attr
+
+        mods = attr.get("mods", [])
+        head = attr.get("data", {}).get("head", [])
+        body = attr.get("data", {}).get("body", [])
+
+        table_class = "table"
+
+        if "bordered" in mods:
+            table_class += " table-bordered"
+
+        if "hover" in mods:
+            table_class += " table-hover"
+
+        if "striped" in mods:
+            table_class += " table-striped"
+
+        html = f"<table class=\"{table_class} pb-2\">"
+
+        if head:
+            thead = "".join("<th scope=\"col\">" + str(col) + "</th>" for col in head)
+
+            html += f"<thead><tr>{thead}</tr></thead>"
+
+        if body:
+            html += "<tbody>"
+
+            for row in body:
+                tr = "".join("<td>" + str(col) + "</td>" for col in row)
+
+                html += f"<tr>{tr}</tr>"
+
+            html += "</tbody>"
+
+        html += "</table>"
+
+        return html
+
+
+if __name__ == "__main__":
+    site = Site(pathlib.Path("."))
+
+    for key, node in site._nodes.items():
+        print(key, node.path)
+
+    for node in site._nodes.values():
+        node.render(site)
